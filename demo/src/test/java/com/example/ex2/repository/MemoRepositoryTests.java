a6b0408937a4054b73f0dcdb0740c1a93574590f
NHN_MinG <nhn@NHNui-MacBookPro-5.local>
2021-01-19 17:48:55 +0900

    chap2_finish

--- demo/src/test/java/com/example/ex2/repository/MemoRepositoryTests.java
+++ demo/src/test/java/com/example/ex2/repository/MemoRepositoryTests.java
@@ -1,13 +1,7 @@
 package com.example.ex2.repository;
 
-import com.example.ex2.entity.Memo;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 @SpringBootTest
 public class MemoRepositoryTests {
@@ -128,6 +122,7 @@ public class MemoRepositoryTests {
     }
      */
 
+    /*
     @Test
     public void testQueryMethodWithPageable() {
         Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
@@ -138,4 +133,18 @@ public class MemoRepositoryTests {
             System.out.println(memo);
         });
     }
+     */
+
+    /*
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteQueryMethod() {
+
+        memoRepository.deleteMemoByMnoLessThan(10L);
+
+    }
+     */
+
+    
 }
