8bd8d8526907950d25aba1503075d34cced64b99
NHN_MinG <nhn@NHNui-MacBookPro-5.local>
2021-01-19 16:44:04 +0900

    select1

--- demo/src/test/java/com/example/ex2/repository/MemoRepositoryTests.java
+++ demo/src/test/java/com/example/ex2/repository/MemoRepositoryTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.stream.IntStream;
+import java.util.Optional;
 
 @SpringBootTest
 public class MemoRepositoryTests {
@@ -13,6 +13,7 @@ public class MemoRepositoryTests {
     @Autowired
     MemoRepository memoRepository;
 
+    /*
     @Test
     public void testInsertDummies() {
         IntStream.rangeClosed(1, 100).forEach(i -> {
@@ -20,4 +21,21 @@ public class MemoRepositoryTests {
             memoRepository.save(memo);
         });
     }
+     */
+
+    @Test
+    public void testSelect() {
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("========================");
+
+        if (result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+
+
 }
