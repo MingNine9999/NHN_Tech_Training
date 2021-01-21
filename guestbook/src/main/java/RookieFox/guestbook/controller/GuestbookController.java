7efa773579d540d419475ae0d6106c8849ec4dee
NHN_MinG <nhn@NHNui-MacBookPro-5.local>
2021-01-21 17:23:08 +0900

    make_guestbook_register+read_page

--- guestbook/src/main/java/RookieFox/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/RookieFox/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package RookieFox.guestbook.controller;
 
+import RookieFox.guestbook.dto.GuestbookDTO;
 import RookieFox.guestbook.dto.PageRequestDTO;
 import RookieFox.guestbook.dto.PageResultDTO;
 import RookieFox.guestbook.entity.Guestbook;
@@ -10,7 +11,10 @@ import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -32,5 +36,32 @@ public class GuestbookController {
         log.info("list...........");
 
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get.........");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto...." + dto);
+
+        Long gno = service.register(dto);
+
+        System.out.println(gno + "!!!!!!!!!!!!!!!!!!!!");
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/read")
+    public void read(Long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model) {
+        log.info("gno: " + gno);
+
+        GuestbookDTO dto = service.read(gno);
+
+        model.addAttribute("dto", dto);
+
     }
 }
